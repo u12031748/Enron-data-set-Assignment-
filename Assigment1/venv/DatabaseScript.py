17113966d4afd8e13922c1082340cc4e2ed258a6
u12031748 <sandk115@gmail.com>
2018-05-05 11:39:54 +0200

    write to database

--- Assigment1/venv/DatabaseScript.py
+++ Assigment1/venv/DatabaseScript.py
@@ -1,12 +1,40 @@
 import pyodbc
+import re
 cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                      "Server=server_name;"
-                      "Database=db_name;"
+                      "Server=DESKTOP-FGO9KMF;"
+                      "Database=Emails;"
                       "Trusted_Connection=yes;")
 
 
 cursor = cnxn.cursor()
-cursor.execute('SELECT * FROM Table')
+cursor.execute('SELECT * FROM Tbl_Emails')
 
-for row in cursor:
-    print('row = %r' % (row,))
+file = open('C:\\Users\\Khumalo\\Desktop\\Enron emails\\testfile.txt', 'r')
+for line in file:
+    if re.match("(.*)Message-ID:(.*)", line):
+        s = line.replace("Message-ID:", "")
+        print(s)
+    if re.match("(.*)Date:(.*)", line):
+        s = line.replace("Message-ID:", "")
+        print(s)
+    if re.match("(.*)From:(.*)", line):
+        s = line.replace("From:", "")
+        print(s)
+    if re.match("(.*)To:(.*)", line):
+        s = line.replace("To:", "")
+        print(s)
+    if re.match("(.*)Message-ID:(.*)", line):
+        s = line.replace("Message-ID:", "")
+        print(s)
+    if re.match("(.*)Message-ID:(.*)", line):
+        s = line.replace("Message-ID:", "")
+        print(s)
+    if re.match("(.*)(X|x)-FileName:(.*)", line):
+            print("\n")
+            print("\n")
+            break
+#cursor.execute('SELECT * FROM Tbl_Emails')
+
+#for row in cursor:
+#    print('row = %r' % (row,))
+
